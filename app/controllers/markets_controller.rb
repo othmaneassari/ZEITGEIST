a8526fd85171c1e84f10d1661053a4c801040e89
Daniel Todd <dtodd2079@gmail.com>
2023-11-18 08:06:29 -0800

    finished the markets methods

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,28 @@
 class MarketsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @markets = Market.all
+  end
+
+  def show
+    @market = market.find(params[:id])
+  end
+
+  def new
+    @market = market.new
+  end
+
+  def create
+    @market = Market.new(market_params)
+    @market.user = current_user
+    @market.save
+    redirect_to market_path(@market)
+  end
+end
+
+private
+
+def game_params
+  params.require(:market).permit(:address, :category, :name, :description )
 end
