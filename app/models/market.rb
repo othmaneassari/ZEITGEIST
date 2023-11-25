a9bbd591a35f8c72d9b2de3142295262f933aec7
Nadia AUGER <nadia.auger0@gmail.com>
2023-11-25 15:44:08 -0500

    geocoding done. You just have to had the api key into your .env file : 'MAPBOX_API_KEY=pk.eyJ1Ij.......'

--- app/models/market.rb
+++ app/models/market.rb
@@ -3,4 +3,7 @@ class Market < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :ingredients, through: :markets_ingredients
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
